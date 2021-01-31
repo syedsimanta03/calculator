b8bc08d8aa69d018e2731cbd95671f30c6ed4ebe
sam <syed.simanta06@gmail.com>
2021-01-31 20:05:08 +0600

    'addm'

--- action.js
+++ action.js
@@ -1,15 +1,27 @@
 function getPercent() {
   const name1 = document.getElementById('yourName').value
   const lName = document.getElementById('trueLove').value
-  var conditions = ['Tom', 'tom', 'Karren', "karren"]
+  var conditions1 = ['Tom', 'tom', 'Karren', "karren"]
+  var conditions2 = ['Jessy', 'jessy', 'Terry', "terry"]
 
   if (
-    conditions.some((el) => name1.includes(el)) &&
-    conditions.some((el) => lName.includes(el))
+    conditions1.some((el) => name1.includes(el)) &&
+    conditions1.some((el) => lName.includes(el))
   ) {
     document.getElementById('percent').innerHTML =
       'You and ' + lName + ' are ' + '100% compatable!'
-  } else {
+  } 
+  
+  else if (
+    conditions2.some((el) => name1.includes(el)) &&
+    conditions2.some((el) => lName.includes(el))
+  ) {
+    document.getElementById('percent').innerHTML =
+      'You and ' + lName + ' are ' + '0% compatable!'
+  } 
+  
+  
+  else {
     document.getElementById('percent').innerHTML =
       'You and ' +
       lName +
